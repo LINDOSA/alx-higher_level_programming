ea12cc14d5b30b9c96b53c1018582f3fad75163c
LINDOSA <detelepathsa@gmail.com>
2023-08-08 23:29:10 +0000

    Done L

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,8 @@
 
 for first_digit in range(10):
     for second_digit in range(first_digit + 1, 10):
-        print(f"{first_digit:02}, {second_digit:02}", end=", ")
-
-print("99")
+        if first_digit == 8 and second_digit == 9:
+            print("{:02}, {:02}".format(first_digit, second_digit))
+        else:
+            print("{:02}, {:02}".format(first_digit, second_digit), end=", ")
 
