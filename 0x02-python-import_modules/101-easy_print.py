6deccc9250855a4985842fb6d1891a640fdf97eb
LINDOSA <detelepathsa@gmail.com>
2023-08-10 22:31:01 +0000

    New__

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
-print('#pythoniscool')
+
+# Import the subprocess module to run external commands
+import subprocess
+
+# Define a command as a list of strings
+command = ["echo", "#pythoniscool"]
+
+# Use the subprocess module to run the defined command
+subprocess.call(command)
 
