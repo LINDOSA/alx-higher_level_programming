f1b3dcc11085b23ffd198ae4fdb005eefec937c6
LINDOSA <detelepathsa@gmail.com>
2023-08-10 21:43:41 +0000

    New__

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    # Import the add function from add_0.py
+    """Display the sum of 1 and 2."""
     from add_0 import add
 
-    # Assign values
     a = 1
     b = 2
-
-    # Calculate the sum
-    sum_result = add(a, b)
-
-    # Display the result
-    print(f"{a} + {b} = {sum_result}")
+    print("{} + {} = {}".format(a, b, add(a, b)))
 
