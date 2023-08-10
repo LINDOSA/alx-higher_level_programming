16569ad1c9c00fd426c22f6d8386c4efd67cbf10
LINDOSA <detelepathsa@gmail.com>
2023-08-10 22:21:08 +0000

    New__

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,36 +1,34 @@
 #!/usr/bin/python3
 
-# Import necessary components
 import sys
-import calculator_1
+from calculator_1 import add, sub, mul, div
 
-# Check the number of command-line arguments
-if len(sys.argv) != 4:
-    print("Usage: {} <num1> <operator> <num2>".format(sys.argv[0]))
-    sys.exit(1)
+def main():
+    operators = ["+", "-", "*", "/"]
+    if len(sys.argv) != 4:
+        print("Usage: {} <num1> <operator> <num2>".format(sys.argv[0]))
+        sys.exit(1)
 
-# Extract command-line arguments
-num1 = int(sys.argv[1])
-operator = sys.argv[2]
-num2 = int(sys.argv[3])
+    num1 = int(sys.argv[1])
+    operator = sys.argv[2]
+    num2 = int(sys.argv[3])
 
-# Define a dictionary of valid operators and corresponding functions
-operators = {
-    "+": calculator_1.add,
-    "-": calculator_1.sub,
-    "*": calculator_1.mul,
-    "/": calculator_1.div
-}
+    if operator not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-# Check if the operator is valid
-if operator in operators:
-    # Perform the arithmetic operation
-    result = operators[operator](num1, num2)
+    result = 0
+    if operator == "+":
+        result = add(num1, num2)
+    elif operator == "-":
+        result = sub(num1, num2)
+    elif operator == "*":
+        result = mul(num1, num2)
+    elif operator == "/":
+        result = div(num1, num2)
+
     print("{:d} {} {:d} = {:d}".format(num1, operator, num2, result))
-else:
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
 
-# Exit with a status code indicating successful execution
-sys.exit(0)
+if __name__ == "__main__":
+    main()
 
