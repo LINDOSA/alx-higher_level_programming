e1a7920e6fd975e1487f60bc407c2a2f1bbfeede
LINDOSA <detelepathsa@gmail.com>
2023-08-29 20:24:33 +0000

    Done

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represents a square with encapsulated size attribute and area calculation."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get the current size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with validation."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate and return the area of the square."""
+        return self.__size * self.__size
+
