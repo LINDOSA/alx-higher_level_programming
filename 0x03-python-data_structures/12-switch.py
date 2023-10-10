278b108005d4e9476ce0be62b0ad0677bfd48515
LINDOSA <detelepathsa@gmail.com>
2023-10-10 08:04:10 -0700

    Bus

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-
-# Initial values
 a = 89
 b = 10
-
-# Swap the values of 'a' and 'b' using tuple unpacking
 a, b = b, a
-
-# Print the swapped values
-print("a={:d} - b={:d}".format(a, b))  # Output: "a=10 - b=89"
+print("a={:d} - b={:d}".format(a, b))
