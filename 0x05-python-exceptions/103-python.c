679212c6ac7df3d3bf60c12da02449911a1147fb
LINDOSA <detelepathsa@gmail.com>
2023-10-23 21:38:53 -0700

    Update2

--- 0x05-python-exceptions/103-python.c
+++ 0x05-python-exceptions/103-python.c
@@ -1,67 +1,47 @@
+/*
+ * File: 103-python.c
+ * Auth: Type Your Name Here
+ */
+
 #include <Python.h>
 
 void print_python_list(PyObject *p);
 void print_python_bytes(PyObject *p);
 void print_python_float(PyObject *p);
 
-void print_python_list(PyObject *p) {
-    if (!PyList_Check(p)) {
-        printf("[ERROR] Invalid List Object\n");
-        return;
-    }
+/**
+ * print_python_list - Prints basic info about Python lists.
+ * @p: A PyObject list object.
+ */
+void print_python_list(PyObject *p)
+{
+	Py_ssize_t size, alloc, i;
+	const char *type;
+	PyListObject *list = (PyListObject *)p;
+	PyVarObject *var = (PyVarObject *)p;
 
-    Py_ssize_t size = PyList_Size(p);
-    PyListObject *list = (PyListObject *)p;
+	size = var->ob_size;
+	alloc = list->allocated;
 
-    printf("[*] Python list info\n");
-    printf("[*] Size of the Python List = %ld\n", size);
-    printf("[*] Allocated = %ld\n", list->allocated);
+	fflush(stdout);
 
-    for (Py_ssize_t i = 0; i < size; i++) {
-        PyObject *item = list->ob_item[i];
-        const char *type = item->ob_type->tp_name;
-        printf("Element %ld: %s\n", i, type);
+	printf("[*] Python list info\n");
+	if (strcmp(p->ob_type->tp_name, "list") != 0)
+	{
+		printf("  [ERROR] Invalid List Object\n");
+		return;
+	}
 
-        if (strcmp(type, "bytes") == 0)
-            print_python_bytes(item);
-        else if (strcmp(type, "float") == 0)
-            print_python_float(item);
-    }
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", alloc);
+
+	for (i = 0; i < size; i++)
+	{
+		type = list->ob_item[i]->ob_type->tp_name;
+		printf("Element %ld: %s\n", i, type);
+		if (strcmp(type, "bytes") == 0)
+			print_python_bytes(list->ob_item[i]);
+		else if (strcmp(type, "float") == 0)
+			print_python_float(list->ob_item[i]);
+	}
 }
-
-void print_python_bytes(PyObject *p) {
-    if (!PyBytes_Check(p)) {
-        printf("[ERROR] Invalid Bytes Object\n");
-        return;
-    }
-
-    Py_ssize_t size = PyBytes_GET_SIZE(p);
-    PyBytesObject *bytes = (PyBytesObject *)p;
-
-    printf("[.] bytes object info\n");
-    printf("  size: %ld\n", size);
-    printf("  trying string: %s\n", bytes->ob_sval);
-
-    if (size > 10)
-        size = 10;
-
-    printf("  first %ld bytes: ", size);
-    for (Py_ssize_t i = 0; i < size; i++) {
-        printf("%02hhx%c", bytes->ob_sval[i], i == size - 1 ? '\n' : ' ');
-    }
-}
-
-void print_python_float(PyObject *p) {
-    if (!PyFloat_Check(p)) {
-        printf("[ERROR] Invalid Float Object\n");
-        return;
-    }
-
-    PyFloatObject *float_obj = (PyFloatObject *)p;
-    char *buffer = PyOS_double_to_string(float_obj->ob_fval, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
-
-    printf("[.] float object info\n");
-    printf("  value: %s\n", buffer);
-    PyMem_Free(buffer);
-}
-
--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-
 def raise_exception():
-    """Raise a TypeError exception with a custom error message."""
-    raise TypeError("This is a custom TypeError")
-
-# Call the function to raise the exception
-raise_exception()
-
+    raise TypeError
